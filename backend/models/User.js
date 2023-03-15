962f7bfd6a574dddd525d22caf12864480db1a67
Dilang <dilang@berkeley.edu>
2023-03-15 13:28:44 -0700

    change username validation

--- backend/models/User.js
+++ backend/models/User.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema({
     username: {
       type: String,
-      required: true
+      required: false
     },
     email: {
       type: String,
