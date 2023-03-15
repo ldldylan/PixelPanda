02912f2f4afe1a63c0c6abfe8818e99a42e75843
kennytram <kennyvtram@gmail.com>
2023-03-15 11:31:22 -0700

    add cart

--- frontend/src/components/Cart/Cart.js
+++ frontend/src/components/Cart/Cart.js
@@ -1,96 +1,96 @@
-import CartItem from "./CartItem/CartItemsIndex"
-import { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { clearCartItems } from "../../store/cartItem";
-import { fetchCartItems } from "../../store/cartItem";
-import { receiveArtwork } from "../../store/artworks";
-import { useHistory } from "react-router-dom";
-import './CartIndex.css'
-import EmptyCart from "./EmptyCart";
+// import CartItem from "./CartItem/CartItemsIndex"
+// import { useEffect, useState } from "react";
+// import { useDispatch, useSelector } from "react-redux";
+// // import { clearCartItems } from "../../store/cartItem";
+// // import { fetchCartItems } from "../../store/cartItem";
+// // import { receiveArtwork } from "../../store/artworks";
+// import { useHistory } from "react-router-dom";
+// // import './CartIndex.css'
+// import EmptyCart from "./EmptyCart";
 
-const Cart = () => {
-    const dispatch = useDispatch();
-    const history = useHistory();
-    const products = useSelector(receiveProducts);
-    const [subtotal, setSubtotal] = useState(0.0);
-    const cartItems = useSelector(state => state.cartItems);
-    const carts = Object.values(cartItems)
-    const allCartItems = products.map((product) => (
-        <div key={product.id} >
-            <CartItem product={product} />
-            <hr />
-        </div>
-    ));
+// const Cart = () => {
+//     const dispatch = useDispatch();
+//     const history = useHistory();
+//     const products = useSelector(receiveProducts);
+//     const [subtotal, setSubtotal] = useState(0.0);
+//     const cartItems = useSelector(state => state.cartItems);
+//     const carts = Object.values(cartItems)
+//     const allCartItems = products.map((product) => (
+//         <div key={product.id} >
+//             <CartItem product={product} />
+//             <hr />
+//         </div>
+//     ));
 
-    useEffect(() => {
-        calculateSubTotal();
-    }, [products]);
+//     useEffect(() => {
+//         calculateSubTotal();
+//     }, [products]);
 
-    useEffect(() => {
-        dispatch(fetchCartItems())
-    }, [dispatch]);
+//     useEffect(() => {
+//         dispatch(fetchCartItems())
+//     }, [dispatch]);
 
-    const calculateCartSize = () => {
-        let size = 0;
-        carts.map(cart => (
-            size += cart.quantity
-        ));
-        return size
-    };
+//     const calculateCartSize = () => {
+//         let size = 0;
+//         carts.map(cart => (
+//             size += cart.quantity
+//         ));
+//         return size
+//     };
 
-    const calculateSubTotal = () => {
-        let subTotal = 0;
-        products.forEach(product => {
-            subTotal += product.quantity * product.price
-        });
-        setSubtotal(Math.round(subTotal * 100) / 100)
-    };
+//     const calculateSubTotal = () => {
+//         let subTotal = 0;
+//         products.forEach(product => {
+//             subTotal += product.quantity * product.price
+//         });
+//         setSubtotal(Math.round(subTotal * 100) / 100)
+//     };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        dispatch(clearCartItems());
-        history.push('/checkout')
-    };
+//     const handleSubmit = (e) => {
+//         e.preventDefault();
+//         dispatch(clearCartItems());
+//         history.push('/checkout')
+//     };
 
-    if (!carts) return null;
-    return (
+//     if (!carts) return null;
+//     return (
 
-        <div className="cart-bg-container">
-            {calculateCartSize() > 0 && (
-                <>
-                    <div className="cart-heading">Shopping Cart</div>
-                    <div className="cart-price-heading">Price</div>
-                    <hr className="top-border" />
-                    <div className="card-item-products">{allCartItems}</div>
-                    <div className="sub-total-container">
-                        Subtotal ({calculateCartSize()}{" "}
-                        {calculateCartSize() > 1 ? "items" : "item"}):&nbsp;
-                        <span className="sub-total-amt">${subtotal}</span>
-                    </div>
-                    <div className="checkout-container">
-                        <div className="sub-total-container">
-                            Subtotal ({calculateCartSize()}{" "}
-                            {calculateCartSize() > 1 ? "items" : "item"}):&nbsp;
-                            <span className="sub-total-amt">${subtotal}</span>
-                        </div>
-                        <form onSubmit={handleSubmit}>
-                            <input
-                                type='submit'
-                                className="checkout-btn"
-                                value="Proceed to Checkout"
-                            ></input>
-                        </form>
-                    </div>
-                </>
-            )}
-            {calculateCartSize() < 1 && (
-                <>
-                    <EmptyCart />
-                </>
-            )}
+//         <div className="cart-bg-container">
+//             {calculateCartSize() > 0 && (
+//                 <>
+//                     <div className="cart-heading">Shopping Cart</div>
+//                     <div className="cart-price-heading">Price</div>
+//                     <hr className="top-border" />
+//                     <div className="card-item-products">{allCartItems}</div>
+//                     <div className="sub-total-container">
+//                         Subtotal ({calculateCartSize()}{" "}
+//                         {calculateCartSize() > 1 ? "items" : "item"}):&nbsp;
+//                         <span className="sub-total-amt">${subtotal}</span>
+//                     </div>
+//                     <div className="checkout-container">
+//                         <div className="sub-total-container">
+//                             Subtotal ({calculateCartSize()}{" "}
+//                             {calculateCartSize() > 1 ? "items" : "item"}):&nbsp;
+//                             <span className="sub-total-amt">${subtotal}</span>
+//                         </div>
+//                         <form onSubmit={handleSubmit}>
+//                             <input
+//                                 type='submit'
+//                                 className="checkout-btn"
+//                                 value="Proceed to Checkout"
+//                             ></input>
+//                         </form>
+//                     </div>
+//                 </>
+//             )}
+//             {calculateCartSize() < 1 && (
+//                 <>
+//                     <EmptyCart />
+//                 </>
+//             )}
 
-        </div>
-    );
-};
+//         </div>
+//     );
+// };
 
-export default Cart
+// export default Cart
