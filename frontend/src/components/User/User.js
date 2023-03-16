10b4c57e3712a7fbda6bf2e8253eec86782c4d2e
kennytram <kennyvtram@gmail.com>
2023-03-16 09:48:24 -0700

    add animation to categories

---
+++ frontend/src/components/User/User.js
@@ -0,0 +1,15 @@
+
+
+function User() {
+
+
+    return (<>
+        <div className="user-show-page">
+            <div className="user-profile">
+                <label className="email-profile">Email: <span></span></label>
+            </div>
+        </div>
+    </>);
+}
+
+export default User;
