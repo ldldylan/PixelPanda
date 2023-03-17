339f889737e5d5727feca41bb9c8fb943cebe2b7
Dilang <dilang@berkeley.edu>
2023-03-16 21:31:13 -0700

    update artwork.jsx

--- frontend/src/components/Artwork/Artwork.jsx
+++ frontend/src/components/Artwork/Artwork.jsx
@@ -3,80 +3,87 @@ import Footer from "../Footer/Footer";
 import { useDispatch } from "react-redux";
 import { useEffect } from "react";
 import { useSelector } from 'react-redux';
-import { useHistory } from "react-router-dom";
 import { useParams } from "react-router-dom";
 import "./Artwork.css";
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
-import { fetchArtworks, fetchArtwork } from "../../store/artworks";
+import { fetchArtworks } from "../../store/artworks";
 import { getArtwork } from "../../store/artworks";
 import { NavLink } from "react-router-dom";
-import CreateReviewPage from "../Review/Create/createReviewForm";
-import { fetchArtworkReviews } from "../../store/reviews";
+import CreateReviewPage from "../Review/Create/createReviewFrom";
+import { fetchArtworkReviews, getReview } from "../../store/reviews";
+import { getReviews } from "../../store/reviews";
 function Artwork() {
     const {artworkId} = useParams();
     const dispatch = useDispatch();
-    const params = useParams();
-    const history = useHistory();
     // const artwork = useSelector (state => state.artworks.artwork);
     // console.log(artworkId)
-
-    const artwork = useSelector(state => state.artworks);
     // console.log(artwork)
     useEffect(()=> {
         dispatch(fetchArtworks())
         dispatch(fetchArtworkReviews(artworkId))
-    },[dispatch]) 
+    },[dispatch])
+    const artwork = useSelector(getArtwork(artworkId));
+    console.log(artwork,'artwork')
+    const reviews = useSelector((state) => state.artworks.reviews);
+    // if (!reviews) {
+    //     return <div>Loading...</div>;
+    // }\
+    console.log(reviews,'reviews???')
+    useEffect(()=> {
+        if (reviews !== undefined){
+        console.log(Object.values(reviews), 'Object.values')
+        console.log('pass')
+        console.log(reviews[0].content,'reviews')
+    }
+    })
     return (
     <>
         <NavBar/>
         <div className="artwork">
              <div className="artwork-image-container">
-                <img 
-                src={artwork?.ArtworkImageUrl ? artwork.ArtworkImageUrl : null}
-                style={{ 
-                backgroundRepeat: "no-repeat", 
-                backgroundSize: "contain",
-                backgroundPosition: "center",
-                objectFit: "cover"  }} 
+                <img style={{
+              backgroundImage: "url('https://aws-mern-pixelpanda.s3.us-west-1.amazonaws.com/public/1678898915410.png')",
+              backgroundRepeat: "no-repeat",
+              backgroundSize: "contain",
+              backgroundPosition: "center" }}
               className="artwork-image"/>
              </div>
              <div className="artwork-purchase">
                 <div className="artwork-title">
                     {artwork?.name ? artwork.name : "Untitled"}
                 </div>
-                <div className="artwork-author" onClick={()=>history.push(`/users/${artwork.author._id}`)}>
-                    {artwork?.author?.email ? artwork.author.email.split('@')[0] : "Mysterious Artist"}
-                </div>
                 <div className="artwork-price">
-                    ${artwork?.price ? artwork.price : "3.50"}
+                    {artwork?.price ? artwork.price : "$3.50"}
                 </div>
-                <div className="artwork-about">
-                    About this item:
-                    <div className="artwork-desc">
-                        {artwork?.description ? artwork.description : "I don't know but I'm sure it is a great product"}
-                    </div>
-                </div>
-                {/* <div className="artwork-cart-buy">
-                    <div className="artwork-cart">
-                    <button>Add to Cart</button>
-                    </div>
+                <div className="artwork-cart-buy">
+                    <div className="artwork-cart"><AddShoppingCartIcon/></div>
                     <div className="artwork-buy-now">Buy Now</div>
-                </div> */}
+                </div>
              </div>
-             {/* <div className="artwork-description">
+             <div className="artwork-description">
                 {artwork?.description ? artwork.description : "Something is supposed to be here..."}
-             </div> */}
+             </div>
              <div className="artwork-comments-container">
                 <ul className="artwork-comments">
                     {artwork?.comments ? artwork.comments.map(comment => (
                         <li key={artwork.comments.id} className="artwork-comment">{comment}</li>
-                    )) : 
+                    )) :
                         <li className="artwork-comment">This is good stuff</li>
-                    }           
+                    }
                 </ul>
              </div>
         </div>
-            <NavLink
+        <div>test1</div>
+            <div>
+                {reviews?.map((review) => (
+                    <div key={review.id}>
+                        <p>{review.content}</p>
+                        <p>{review.rating}</p>
+                    </div>
+                ))}
+            </div>
+            <div>test2</div>
+                   <NavLink
                 className="link-to-create-review"
                 to={`/artworks/${artworkId}/review`}
             >
@@ -86,5 +93,4 @@ function Artwork() {
     </>
     );
 }
-
 export default Artwork;
