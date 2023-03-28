776a8ee16d4592c0ec2e8cfee20954f79c62a9a8
Jameswu <wuyuwen0@gmail.com>
2023-03-28 16:25:37 -0700

    add instruction on upload image

--- frontend/src/components/Artwork/Create/CreateArtworkPage.jsx
+++ frontend/src/components/Artwork/Create/CreateArtworkPage.jsx
@@ -218,6 +218,8 @@ export default function CreateArtworkPage({ onClose, updateShouldFetchArtworks }
                         accept=".jpg, .jpeg, .png"
                         onChange={updateFile} />
                     <p><i class="fa-solid fa-cloud-arrow-up"></i></p>
+                    <p className='uploadins'>click to upload</p>
+                    <p className='recommend'>We recommend using high-quality .jpg files under 20MB</p>
                 </div>
                 
                 <label className= {errors.category ? "error category-label" : "category-label"} htmlFor="category">Category:
