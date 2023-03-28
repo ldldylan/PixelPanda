355ca51299b0ef5bd72301b77e51fb2b99d6a4ac
kennytram <kennyvtram@gmail.com>
2023-03-28 00:16:53 -0700

    remove user attribute from the cartItem model

--- backend/models/CartItem.js
+++ backend/models/CartItem.js
@@ -2,10 +2,6 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const cartItemSchema = new Schema({
-    user: {
-        type: Schema.Types.ObjectId,
-        ref: 'User'
-    },
     artwork: {
         type: Schema.Types.ObjectId,
         ref: 'Artwork'
