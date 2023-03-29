3d25038ff01468f64f10917a0a3d330d6cee9fda
kennytram <kennyvtram@gmail.com>
2023-03-29 16:33:31 -0700

    remove unnecessary useEffect that causese artworks to rerender

--- frontend/src/components/NavBar/NavBar.js
+++ frontend/src/components/NavBar/NavBar.js
@@ -34,8 +34,6 @@ function NavBar({ updateShouldFetchArtworks }) {
 
   const searchResultsRef = useRef(null);
 
-  const searchResultsPortal = document.createElement("div");
-
   const searchItems = (searchValue) => {
     setSearchInput(searchValue);
 
@@ -67,15 +65,6 @@ function NavBar({ updateShouldFetchArtworks }) {
     e.preventDefault();
     history.push('/cart')
   }
-
-  useEffect(() => {
-    document.body.appendChild(searchResultsPortal);
-
-    // remove the portal element from the document body when the component unmounts
-    return () => {
-      document.body.removeChild(searchResultsPortal);
-    };
-  }, [searchResultsPortal]);
 
   useEffect(() => {
     const handleClickOutside = (e) => {
