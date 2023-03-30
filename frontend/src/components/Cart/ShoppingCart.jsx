d6b0b5ee5d7557bb5c26b60d900431a7d6229b98
kennytram <kennyvtram@gmail.com>
2023-03-29 17:17:39 -0700

    add styles to checkout_success

--- frontend/src/components/Cart/ShoppingCart.jsx
+++ frontend/src/components/Cart/ShoppingCart.jsx
@@ -6,8 +6,8 @@ import { useHistory } from "react-router-dom";
 import './Cart.css'
 import { fetchArtworks } from "../../store/artworks";
 import { getCartItems } from "../../store/cartItems";
-import {fetchUserCartItems} from "../../store/cartItems";
-import {deleteAllCartItems, checkoutCartItems} from "../../store/cartItems";
+import { fetchUserCartItems } from "../../store/cartItems";
+import { deleteAllCartItems, checkoutCartItems } from "../../store/cartItems";
 import NavBar from "../NavBar/NavBar";
 import Footer from "../Footer/Footer";
 import { deleteCartItem, clearCart } from "../../store/cartItems";
@@ -23,7 +23,7 @@ const Cart = () => {
     const [matchingArtworks, setMatchingArtworks] = useState([]);
     const [loaded, setLoaded] = useState(false);
 
-    
+
     useEffect(() => {
         Promise.all([
             dispatch(fetchArtworks()),
@@ -32,7 +32,7 @@ const Cart = () => {
             setLoaded(true);
         });
     }, [dispatch, currentUser]);
-    
+
     const calculateSubTotal = () => {
         let sumPrice = 0
         const matchingArtworks = cartItems
@@ -40,21 +40,21 @@ const Cart = () => {
             .filter((artwork) => artwork !== undefined);
         setMatchingArtworks(matchingArtworks);
         console.log(matchingArtworks);
-        if (matchingArtworks.length === 0){
+        if (matchingArtworks.length === 0) {
             setSubTotal(0);
             return;
         }
 
         matchingArtworks.forEach(artwork => {
-            if(artwork?.price) sumPrice += artwork.price
-        }) 
-        setSubTotal(Math.round(sumPrice * 100) /100)
+            if (artwork?.price) sumPrice += artwork.price
+        })
+        setSubTotal(Math.round(sumPrice * 100) / 100)
 
     };
 
     useEffect(() => {
         calculateSubTotal();
-        
+
     }, [cartItems, artworks]);
 
     const handleCheckout = (e) => {
@@ -65,11 +65,11 @@ const Cart = () => {
         // alert("Thank you for your purchase! Your order is being processed.")
         // history.push('/');
     };
-    
+
     const handleDeteCartItem = cartArtworkId => (e) => {
         e.preventDefault();
-        for(let i=0;i<cartItems.length;i++){
-            if(cartItems[i].artwork === cartArtworkId){
+        for (let i = 0; i < cartItems.length; i++) {
+            if (cartItems[i].artwork === cartArtworkId) {
                 dispatch(deleteCartItem(cartItems[i]._id))
                 history.push('/cart')
             }
@@ -84,99 +84,100 @@ const Cart = () => {
 
         )
     } else {
-    return(
-        <>
-        <NavBar/>
-        <div className="cart-page">
-            {Object.keys(cartItems).length > 0 && (
-                <div className="cart-container">
-                    <div className="cart-content">
-                        <div className="cart-item-box">
-                            <div className="cart-item-header">
-                                <div className="cart-heading">{cartItems.length} item(s) in your shopping cart</div>
-                                <div className="cart-price-heading">Price</div>
-                            </div>
-                            <div style={{marginTop: "10px", marginBottom: "10px"}}></div>
-                            <div>
-                                {matchingArtworks.map((cartArtwork) => (
-                                    <div key={cartArtwork._id} className="cart-item">
-                                        <div className="cart-item-info">
-                                            <div className="cart-item-img">
-                                                <img src={cartArtwork?.ArtworkImageUrl ? cartArtwork.ArtworkImageUrl : null} style={{
-                                                    backgroundRepeat: "no-repeat",
-                                                    backgroundSize: "contain",
-                                                    backgroundPosition: "center",
-                                                    objectFit: "cover"}}
-                                                    className="cart-item-preview-image"
-                                                    onClick={() => history.push(`/artworks/${cartArtwork._id}`)}/>
-                                            </div>
-                                            <div className="cart-item-details">
-                                                    <div className="cart-item-title">{cartArtwork?.name ? cartArtwork.name : null}</div>
-                                                    <div className="cart-item-author" onClick={()=> history.push(`/users/${cartArtwork.author._id}`)}>By artist: {cartArtwork?.author.email ? cartArtwork.author.email.split('@')[0] : null}</div>
-                                                    <div className="cart-item-delete-btn" onClick={handleDeteCartItem(cartArtwork._id)}>remove item from cart</div>
-                                            </div>
-                                            <div>
-
-                                            </div>
-                                            <div className="cart-item-price">
-                                                ${cartArtwork?.price ? cartArtwork.price.toFixed(2) : null}
-                                            </div>
-                                        </div>
+        return (
+            <>
+                <NavBar />
+                <div className="cart-page">
+                    {Object.keys(cartItems).length > 0 && (
+                        <div className="cart-container">
+                            <div className="cart-content">
+                                <div className="cart-item-box">
+                                    <div className="cart-item-header">
+                                        <div className="cart-heading">{cartItems.length} item(s) in your shopping cart</div>
+                                        <div className="cart-price-heading">Price</div>
                                     </div>
-                                ))}
+                                    <div style={{ marginTop: "10px", marginBottom: "10px" }}></div>
+                                    <div>
+                                        {matchingArtworks.map((cartArtwork) => (
+                                            <div key={cartArtwork._id} className="cart-item">
+                                                <div className="cart-item-info">
+                                                    <div className="cart-item-img">
+                                                        <img src={cartArtwork?.ArtworkImageUrl ? cartArtwork.ArtworkImageUrl : null} style={{
+                                                            backgroundRepeat: "no-repeat",
+                                                            backgroundSize: "contain",
+                                                            backgroundPosition: "center",
+                                                            objectFit: "cover"
+                                                        }}
+                                                            className="cart-item-preview-image"
+                                                            onClick={() => history.push(`/artworks/${cartArtwork._id}`)} />
+                                                    </div>
+                                                    <div className="cart-item-details">
+                                                        <div className="cart-item-title">{cartArtwork?.name ? cartArtwork.name : null}</div>
+                                                        <div className="cart-item-author" onClick={() => history.push(`/users/${cartArtwork.author._id}`)}>By artist: {cartArtwork?.author.email ? cartArtwork.author.email.split('@')[0] : null}</div>
+                                                        <div className="cart-item-delete-btn" onClick={handleDeteCartItem(cartArtwork._id)}>remove item from cart</div>
+                                                    </div>
+                                                    <div>
 
-                            </div>
-                            <div style={{ marginTop: "10px", marginBottom: "10px" }}></div>
-                        </div>
-                        <div className="checkout-box">
-                            <div className="checkout-container">
-                                <div className="sub-total-container">
-                                    Subtotal ({Object.keys(cartItems).length}{" "}
-                                    {cartItems.length > 1 ? "items" : "item"}):&nbsp;
-                                    ${subTotal}
+                                                    </div>
+                                                    <div className="cart-item-price">
+                                                        ${cartArtwork?.price ? cartArtwork.price.toFixed(2) : null}
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        ))}
+
+                                    </div>
+                                    <div style={{ marginTop: "10px", marginBottom: "10px" }}></div>
                                 </div>
-                                <form onSubmit={handleCheckout} className="checkout-form">
-                                    <input
-                                        type='submit'
-                                        className="checkout-btn"
-                                        value="Proceed to Checkout"
-                                    ></input>
-                                </form>
+                                <div className="checkout-box">
+                                    <div className="checkout-container">
+                                        <div className="sub-total-container">
+                                            Subtotal ({Object.keys(cartItems).length}{" "}
+                                            {cartItems.length > 1 ? "items" : "item"}):&nbsp;
+                                            ${subTotal}
+                                        </div>
+                                        <form onSubmit={handleCheckout} className="checkout-form">
+                                            <input
+                                                type='submit'
+                                                className="checkout-btn"
+                                                value="Proceed to Checkout"
+                                            ></input>
+                                        </form>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                    </div>
-                    <hr className="top-border" />
-                    {/* <div className="card-item-artworks">{allCartItems}</div> */}
-{/* 
+                            <hr className="top-border" />
+                            {/* <div className="card-item-artworks">{allCartItems}</div> */}
+                            {/* 
                     <div className="sub-total-container">
                         Subtotal ({Object.keys(cartItems).length}{" "}
                         {Object.keys(cartItems).length > 1 ? "items" : "item"}):&nbsp;
                         <span className="sub-total-amt">${subTotal}</span>
                     </div> */}
 
-                    
+
+                        </div>
+                    )}
+                    {Object.keys(cartItems).length < 1 && (
+                        <div className="empty-cart-container">
+                            <div className="panda-box-container">
+                                <a href="/"><div className="panda-box"></div></a>
+                            </div>
+                            <div className="empty-cart-heading">
+                                Your cart is empty
+                            </div>
+                            <div className="empty-cart-text">
+                                Looks like you haven't added anything to your cart yet.
+                            </div>
+                            <div className="empty-cart-mainpage-link">
+                                <a href="/">👉Start shopping👈</a>
+                            </div>
+                        </div>
+                    )}
                 </div>
-            )}
-            {Object.keys(cartItems).length < 1 && (
-                <div className="empty-cart-container">
-                    <div className="panda-box-container">
-                    <a href="/"><div className="panda-box"></div></a>
-                    </div>
-                    <div className="empty-cart-heading">
-                        Your cart is empty
-                    </div>
-                    <div className="empty-cart-text">
-                        Looks like you haven't added anything to your cart yet.
-                    </div>
-                    <div className="empty-cart-mainpage-link">
-                        <a href="/">👉Start shopping👈</a>
-                    </div>
-                </div>
-            )}
-        </div>
-        <div id='cart-page-footer'><Footer/></div>
-        </>
-    )
+                <div id='cart-page-footer'><Footer /></div>
+            </>
+        )
     }
 };
 
