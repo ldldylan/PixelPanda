d30f9c25c5fb3c26c2c1d5f90ee16f56c8c21974
Dilang <dilang@berkeley.edu>
2023-03-30 22:00:05 -0700

    style about page

--- frontend/src/components/AboutPage/About.js
+++ frontend/src/components/AboutPage/About.js
@@ -10,17 +10,26 @@ function About(){
             <div className='team-member-container'>
                 <div className='team-member'>
                     <div className='member-image-box'><img className='member-image' src={dilang}/></div>
-                    <div className='member-text'><p>Exquis is a collaborative story-telling app, where users can create “skeletons” and add collaborators, who take turns adding “bones” to the skeleton. Exquis is inspired by the surrealist technique known as “exquisite corpse,” where participants take turns adding piece by piece to a dynamic, evolving collective art piece. At Exquis we believe more minds = more fun. Watch your skeletons grow and unfold into unpredictable and unique works of art. Build beautiful stories and experiences with friends, or build friendships by creating art together.</p></div>
+                    <div className='member-description'>
+                        <div className='member-name'><h1>Dilang Lin</h1></div>
+                        <div className='member-text'><p></p></div>
+                    </div>
                     <div className='empty-div'></div>
                 </div>
                 <div className='team-member'>
                     <div className='empty-div'></div>
-                    <div className='member-text'><p>Exquis is a collaborative story-telling app, where users can create “skeletons” and add collaborators, who take turns adding “bones” to the skeleton. Exquis is inspired by the surrealist technique known as “exquisite corpse,” where participants take turns adding piece by piece to a dynamic, evolving collective art piece. At Exquis we believe more minds = more fun. Watch your skeletons grow and unfold into unpredictable and unique works of art. Build beautiful stories and experiences with friends, or build friendships by creating art together.</p></div>
+                    <div className='member-description'>
+                        <div className='member-name'><h1>Kenny Tram</h1></div>
+                        <div className='member-text'><p>About yourself or project duties go here? About yourself or project duties go here? About yourself or project duties go here? About yourself or project duties go here? About yourself or project duties go here? About yourself or project duties go here? About yourself or project duties go here?</p></div>
+                    </div>
                     <div className='member-image-box'><img className='member-image' src={kenny} /></div>
                 </div>
                 <div className='team-member'>
-                    <div className='member-image-box'><img className='member-image' src={dilang} /></div>
-                    <div className='member-text'><p>Exquis is a collaborative story-telling app, where users can create “skeletons” and add collaborators, who take turns adding “bones” to the skeleton. Exquis is inspired by the surrealist technique known as “exquisite corpse,” where participants take turns adding piece by piece to a dynamic, evolving collective art piece. At Exquis we believe more minds = more fun. Watch your skeletons grow and unfold into unpredictable and unique works of art. Build beautiful stories and experiences with friends, or build friendships by creating art together.</p></div>
+                    <div className='member-image-box'><img className='member-image' src={kenny} /></div>
+                    <div className='member-description'>
+                        <div className='member-name'><h1>James Wu</h1></div>
+                        <div className='member-text'><p>About yourself or project duties go here? About yourself or project duties go here? About yourself or project duties go here? About yourself or project duties go here? About yourself or project duties go here? About yourself or project duties go here? About yourself or project duties go here?</p></div>
+                    </div>
                     <div className='empty-div'></div>
                 </div>
             </div>
